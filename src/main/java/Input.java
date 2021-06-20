7616fb10021df25326a03c9fd144f25ee55bcf89
jwr-tube <jwrtube@gmail.com>
2021-06-20 16:21:58 +0530

    Inputs

---
+++ src/main/java/Input.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name:");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age :");
+        String age = scanner.nextLine();
+
+        System.out.print("enter your address:");
+        String address = scanner.nextLine();
+
+        System.out.println("-----result-------");
+        System.out.println("Your name is :"+name);
+        System.out.println("Your age is  :"+age);
+        System.out.print("Your address is :"+address);
+    }
+}
