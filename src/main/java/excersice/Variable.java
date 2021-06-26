d9c4bd408232b2111fee251b9962a9b12e52f304
jwr-tube <jwrtube@gmail.com>
2021-06-26 18:27:40 +0530

    Variables

---
+++ src/main/java/excersice/Variable.java
@@ -0,0 +1,33 @@
+package excersice;
+
+public class Variable {
+    public static void main(String[] args) {
+        /**
+         * Java
+         */
+
+//        Define a variable named number1 by assigning 40 to it.
+        int number1 = 40;
+//        Define a variable named number2 by assigning 20 to it.
+        int number2 = 20;
+//        Declare a variable named result;
+        int result;
+//        Add them (number1 + number2) and assign to result variable
+        result = number1+number2;
+//        Print the result variable.
+        System.out.println(result);
+//        Get the difference of them (number1 - number2) and assign to result
+        result = number1-number2;
+//        Print the result variable.
+        System.out.println(result);
+//        Multiply them (number1 * number2) and assign to result variable
+        result = number1*number2;
+//        Print the result variable.
+        System.out.println(result);
+//        Divide them (number1 / number2) and assign to result variable
+        result = number1/number2;
+//        Print the result variable.
+        System.out.println(result);
+    }
+}
+
