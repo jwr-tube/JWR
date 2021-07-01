aa5a804e697e1331d9e19ce089351327cf1d7b68
jwr-tube <jwrtube@gmail.com>
2021-07-01 07:31:58 +0530

    Data types excersize

---
+++ src/main/java/excersice/DataTypesSized.java
@@ -0,0 +1,13 @@
+package excersice;
+
+public class DataTypesSized {
+    public static void main(String[] args) {
+        System.out.println(" byte: SIZE  :"+Byte.SIZE+"  MIN :"+Byte.MIN_VALUE+" MAX :"+Byte.MAX_VALUE);
+        System.out.println(" short: SIZE :"+Short.SIZE+" MIN :"+Short.MIN_VALUE+" MAX :"+Short.MAX_VALUE);
+        System.out.println(" int: SIZE   :"+Integer.SIZE+" MIN :"+Integer.MIN_VALUE+" MAX :"+Integer.MAX_VALUE);
+        System.out.println(" long: SIZE  :"+Long.SIZE+" MIN :"+Long.MIN_VALUE+" MAX :"+Integer.MAX_VALUE);
+        System.out.println(" float: SIZE :"+Float.SIZE+" MIN :"+Float.MIN_VALUE+" MAX :"+Float.MAX_VALUE);
+        System.out.println(" double:SIZE :"+Double.SIZE+" MIN :"+Double.MIN_VALUE+" MAX :"+Double.MAX_VALUE);
+        System.out.println(" char: SIZE  :"+Character.SIZE+" MIN :"+Character.MIN_VALUE+" MAX :"+Character.MAX_VALUE);
+    }
+}
